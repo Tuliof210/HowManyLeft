4411b1a64d30f4c45f0a3049d5f4062d27f757a6
Tulio Ferreira <tuliof210@gmail.com>
2021-04-10 20:06:29 -0300

    rename project

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,22 @@
 const main = document.querySelector('main');
 const screens = {};
-
-//-----------------------------------------
+//-------------------------------------------
+const alarms = [];
+//-------------------------------------------
 (() => start())();
 function start() {
-  loadHome();
+  displayHome();
 }
 //===========================================================================
-async function loadHome() {
+async function displayHome() {
   if (screens['home']) main['innerHTML'] = screens['home'];
   else main['innerHTML'] = await fetchData('../templates/home.html', 'home');
 }
-async function loadList() {
+async function displayList() {
   if (screens['list']) main['innerHTML'] = screens['list'];
   else main['innerHTML'] = await fetchData('../templates/list.html', 'list');
 }
-async function loadEdit() {
+async function displayEdit() {
   if (screens['edit']) main['innerHTML'] = screens['edit'];
   else main['innerHTML'] = await fetchData('../templates/edit.html', 'edit');
 }
