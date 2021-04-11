6a41abe661ae9a13633e92ad81030836c6d8feaf
Tulio Ferreira <tuliof210@gmail.com>
2021-04-10 22:35:15 -0300

    template rows

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,35 @@
 const main = document.querySelector('main');
 const screens = {};
 //-------------------------------------------
-const reminders = [];
+const reminders = [
+  {
+    id: '46543434',
+    title: 'Entregar essa bosta',
+    unstructuredDate: {
+      days: 1,
+      hours: 9,
+      minutes: 45,
+      seconds: 45,
+    },
+  },
+  {
+    id: '139539',
+    title: 'Aniversario Muzin',
+    unstructuredDate: {
+      days: 45,
+      hours: 3,
+      minutes: 11,
+      seconds: 28,
+    },
+  },
+];
 //-------------------------------------------
 (() => start())();
 function start() {
   displayHome();
+}
+function appHelp() {
+  alert('teste');
 }
 //===========================================================================
 async function displayHome() {
@@ -15,12 +39,17 @@ async function displayHome() {
 async function displayList() {
   if (screens['list']) main['innerHTML'] = screens['list'];
   else main['innerHTML'] = await fetchData('../templates/list.html', 'list');
+
+  populateReminders();
 }
 async function displayEdit() {
   if (screens['edit']) main['innerHTML'] = screens['edit'];
   else main['innerHTML'] = await fetchData('../templates/edit.html', 'edit');
 }
 //============================================================================
-function appHelp() {
-  alert('teste');
+
+function populateReminders() {
+  let remindersHTML = ``;
+  reminders.forEach(reminder => {});
+  const listWrapper = document.querySelector('.list-main');
 }
